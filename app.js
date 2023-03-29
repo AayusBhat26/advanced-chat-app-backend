0a77e802539022c23afda77e85580643fc39dabd
AayusBhat26 <100667276+AayusBhat26@users.noreply.github.com>
2023-03-30 00:08:19 +0530

    setting up backend

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+// information regarding to request send and response receive in terminal
+const morgan = require("morgan");
+
+const app = express();
+
+// exporting express app.
+module.exports = app;
